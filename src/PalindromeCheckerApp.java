d6a6548a47999155203486020462662dab2fa63d
Yash <mathury2006@gmail.com>
2026-02-12 16:06:48 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+/*
+  *UC1 Palindrome Checker
+  *Application Entry & Welcome Message
+
+  *Description:Entry point of Palindrome Checker Management System
+
+  *@author Yash
+  *@version 1.0
+ */
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System Initialized Successfully");
+    }
+}
