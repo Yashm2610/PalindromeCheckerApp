fab2ee3d876f021ca70b3aa5f756e6abb0edb61f
Yashm2610 <mathury2006@gmail.com>
2026-03-06 14:49:33 +0530

    Performance Comparison

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,48 +1,67 @@
-import java.util.Scanner;
+import java.util.*;
 
-// Service class that contains palindrome logic
-class PalindromeChecker {
+public class PalindromeCheckerApp {
 
-    // Method to check palindrome
-    public boolean checkPalindrome(String str) {
+    // Method 1: Two-pointer method
+    public static boolean checkPalindromeTwoPointer(String str) {
 
         int start = 0;
         int end = str.length() - 1;
 
         while (start < end) {
-
             if (str.charAt(start) != str.charAt(end)) {
                 return false;
             }
-
             start++;
             end--;
         }
 
         return true;
     }
-}
 
-// Main application class
-public class PalindromeCheckerApp {
+    // Method 2: Stack method
+    public static boolean checkPalindromeStack(String str) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : str.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a string : ");
+        System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Create object of service class
-        PalindromeChecker checker = new PalindromeChecker();
+        // Measure Two-Pointer Method
+        long start1 = System.nanoTime();
+        boolean result1 = checkPalindromeTwoPointer(input);
+        long end1 = System.nanoTime();
 
-        boolean result = checker.checkPalindrome(input);
+        // Measure Stack Method
+        long start2 = System.nanoTime();
+        boolean result2 = checkPalindromeStack(input);
+        long end2 = System.nanoTime();
 
-        if (result) {
-            System.out.println("The given string is a Palindrome.");
-        } else {
-            System.out.println("The given string is NOT a Palindrome.");
-        }
+        long time1 = end1 - start1;
+        long time2 = end2 - start2;
+
+        System.out.println("\nTwo Pointer Method: " + result1);
+        System.out.println("Execution Time: " + time1 + " ns");
+
+        System.out.println("\nStack Method: " + result2);
+        System.out.println("Execution Time: " + time2 + " ns");
 
         sc.close();
     }
